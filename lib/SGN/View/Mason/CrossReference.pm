f08d44bb505b971f4a51f137764fde8f74c33994
Robert Buels <rmb32@cornell.edu>
2011-08-19 14:42:52 -0700

    make xref rendering use xref tags to categorize xrefs instead of their subsite names

--- lib/SGN/View/Mason/CrossReference.pm
+++ lib/SGN/View/Mason/CrossReference.pm
@@ -8,16 +8,18 @@ use base 'Exporter';
 our @EXPORT_OK = qw( resolve_xref_component );
 
 sub resolve_xref_component {
-    my ( $m, $subsite, $comp_pattern ) = @_;
+    my ( $m, $tags, $comp_pattern ) = @_;
 
-    for my $fname ( $subsite->name, 'default' ) {
+    $tags = [$tags] unless ref $tags;
+
+    for my $fname ( @$tags, 'default' ) {
         my $comp = $comp_pattern;
         $comp =~ s/(?<!%)%f/$fname/g;
 
         return $comp if $m->comp_exists( $comp );
     }
 
-    croak "Cannot find Mason component for pattern '$comp_pattern' subsite ".$subsite->name;
+    croak "Cannot find Mason component for pattern '$comp_pattern' and tags (@$tags)";
 }
 
 
