2dc6fc7caa50338c49b4458d9a65fc11a4f5778c
Robert Buels <rmb32@cornell.edu>
2011-10-07 17:09:48 -0700

    add info about seqfeatures and preview images to xref json

--- lib/SGN/SiteFeatures/CrossReference/WithPreviewImage.pm
+++ lib/SGN/SiteFeatures/CrossReference/WithPreviewImage.pm
@@ -4,5 +4,11 @@ use MooseX::Types::URI qw/ Uri /;
 
 has 'preview_image_url' => ( is => 'ro', isa => Uri, coerce => 1 );
 
+around 'TO_JSON' => sub {
+    my ( $orig, $self ) = @_;
+    my $j = $self->$orig();
+    $j->{preview_image_url} = ''.$self->preview_image_url;
+    $j
+};
 
 1;
--- lib/SGN/SiteFeatures/CrossReference/WithSeqFeatures.pm
+++ lib/SGN/SiteFeatures/CrossReference/WithSeqFeatures.pm
@@ -7,4 +7,21 @@ has 'seqfeatures' => (
     predicate => 'has_seqfeatures',
    );
 
+around 'TO_JSON' => sub {
+    my ( $orig, $self ) = @_;
+    my $j = $self->$orig();
+    $j->{seqfeatures} = [
+        map +{
+            display_name => $_->name,
+            primary_id   => $_->primary_id,
+            start        => $_->start,
+            end          => $_->end,
+            seq_id       => $_->seq_id,
+        },
+        @{$self->seqfeatures}
+    ];
+    $j
+};
+
+
 1;
