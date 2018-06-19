c6a4182ec65a2666036f196cb63f0dbb05b62da3
Vagrant Default User <vagrant@contrib-jessie.vagrantup.com>
2018-06-19 13:42:50 +0000

    Running controller

--- lib/SGN/Controller/HaplotypeVis/Webpage.pm
+++ lib/SGN/Controller/HaplotypeVis/Webpage.pm
@@ -1,7 +1,19 @@
-package SGN::Controller::BreedersToolbox::Webpage;
+package SGN::Controller::HaplotypeVis::Webpage;
 
 use Moose;
 
 BEGIN { extends 'Catalyst::Controller'; }
+use URI::FromHash 'uri';
+
+sub haplotype_vis_input :Path('/haplotype_visualizer') Args(0) {
+    my $self = shift;
+    my $c = shift;
+
+    if (! $c->user) {
+	$c->res->redirect(uri( path => '/user/login', query => { goto_url => $c->req->uri->path_query } ) );
+	return;
+    }
+    $c->stash->{template} = '/haplotype_vis/index.mas';
+}
 
 1;
